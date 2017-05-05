3dae8690000bcae957a7c21df67cabce2e2f9299
Robert Cooper <RobertJohnCooper@Gmail.com>
2017-05-05 12:57:21 +0100

    first

---
+++ db_flask/manage.py
@@ -0,0 +1,96 @@
+# from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cpd.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class Disposal(db.Model):
+    email_id = db.Column(db.Integer, primary_key=True)
+    location = db.Column(db.String(120))
+    date_posted = db.Column(db.Date)
+    rent = db.Column(db.Float)
+    size = db.Column(db.String(30))
+    size_max = db.Column(db.Float)
+    size_min = db.Column(db.Float)
+    lease = db.Column(db.String(120))
+    rates = db.Column(db.Float)
+    service = db.Column(db.Float)
+    address = db.Column(db.String(120))
+    lng = db.Column(db.Float)
+    lat = db.Column(db.Float)
+    post_code = db.Column(db.String(10))
+    area_code = db.Column(db.String(5))
+
+    def __init__(self, email_id, location, date_posted, rent, size, size_max, size_min,
+                 size_avg, address, lng, lat, post_code, area_code, lease, rates, service):
+        self.email_id = email_id
+        self.location = location
+        self.size = size
+        self.size_max = size_max
+        self.size_avg = size_avg
+        self.size_min = size_min
+        self.rent = rent
+        self.date_posted = date_posted
+        self.lease = lease
+        self.rates = rates
+        self.service = service
+        self.address = address
+        self.lng = lng
+        self.lat = lat
+        self.post_code = post_code
+        self.area_code = area_code
+
+    def __repr__(self):
+        s = \
+            '<Location {}>' + \
+            '<date_posted {}>' + \
+            '<service {}>'
+        return s.format(self.location, self.date_posted, self.service)
+
+    def to_dict(self):
+        d = self.__dict__
+        if '_sa_instance_state' in d:
+            d.pop('_sa_instance_state')
+        return d
+
+
+class Acquisition(db.Model):
+    email_id = db.Column(db.Integer, primary_key=True)
+    budget_min = db.Column(db.Float)
+    budget_max = db.Column(db.Float)
+    size_max = db.Column(db.Float)
+    size_min = db.Column(db.Float)
+    areas = db.Column(db.String(120))
+
+    def __init__(self, email_id, budget_min, budget_max, areas, size_max, size_min):
+        self.areas = areas
+        self.budget_min = budget_min
+        self.budget_max = budget_max
+        self.size_max = size_max
+        self.size_min = size_min
+        self.email_id = email_id
+
+
+class AcquisitionAreas(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email_id = db.Column(db.Integer)
+    description = db.Column(db.String(60))
+
+    def __init__(self, email_id, description):
+        self.email_id = email_id
+        self.description = description
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,144 @@
+
+import re
+import pandas as pd
+from db_flask.manage import Disposal, db
+import email_service
+from scraper import regex_dict, extract_field
+from datetime import datetime as dt
+from geopy.geocoders import Nominatim
+import geocoder as gcd
+import time
+
+import locale
+locale.setlocale(locale.LC_ALL, 'uk')
+geo = Nominatim()
+esx = email_service.Extractor('robert.cooper@peppercorn.london', 'C0ntent123qwerty')
+
+
+def extract_postcode(s):
+    try:
+        rgx = re.search('[a-zA-Z]{1,2}\d{1,2}([a-zA-Z])?(\s)?\d{1,2}[a-zA-Z]{2}', s)
+        return rgx.group()
+    except Exception:
+        print('Extract post code issue {}'.format(s))
+        return None
+
+
+def extract_areacode(s):
+    try:
+        rgx = re.search('[a-zA-Z]{1,2}\d{1,2}[a-zA-Z]?', s)
+        return rgx.group()
+    except Exception:
+        print('Extract area code issue {}'.format(s))
+        return None
+
+
+def _clean_extract(s):
+    if isinstance(s, str):
+        try:
+            d = locale.atof(s)
+            return d
+        except Exception:
+            return s.replace('\\r\\n', '').replace('\\xe2\\x80\\x93', '-').replace('\\x96', '-').lstrip().rstrip()
+
+
+def geocode_dic(loc, pc, ac):
+    time.sleep(0.5)
+    gg = gcd.google(loc + ', London')
+    while gg.status != 'ZERO_RESULTS':
+        print(gg.status)
+        if gg.status is 'OK':
+            if not pc:
+                pc = gg.postal
+            return {
+                'address': gg.address,
+                'lng': gg.lng,
+                'lat': gg.lat,
+                'post_code': pc,
+                'area_code': ac}
+        if gg.status is 'OVER_QUERY_LIMIT':
+            break
+    if pc is not loc:
+        return geocode_dic(pc, pc, ac)
+    if ac is not loc:
+        return geocode_dic(ac, pc, ac)
+
+
+def update_database():
+
+    print('***Fetching Email***')
+    mbs = esx.message_bodies()
+    for k, m in mbs.items():
+        # print('--- Processing Email with UID {}'.format(k) + '-------------------------------------------------')
+        listing_type = determine_listing_type(m)
+        if listing_type == 'Acquisition':
+            pass
+            # rpt = upload_acquisition(m)
+            # if not rpt:
+            #     print('!!! Upload failed for UID {} !!!'.format(k))
+            #     continue
+        elif listing_type == 'Disposal':
+            rpt = upload_disposal(m, k)
+            if rpt:
+                print('Database updated with UID {}'.format(k))
+            else:
+                pass
+
+
+def determine_listing_type(msg):
+    if extract_field(('New disposal posted!', 0), msg):  # is not None:
+        return 'Disposal'
+    elif extract_field(('New acquisition posted!', 0), msg):  # is not None:
+        return 'Acquisition'
+    else:
+        return 'Unrecognised'
+
+
+def upload_acquisition(msg):
+    efs = {f: (extract_field(v, msg)) for f, v in regex_dict.items()}
+
+    if not efs['location']:
+        return False
+
+    return True
+
+
+def upload_disposal(msg, uid):
+    efs = {f: (extract_field(v, msg)) for f, v in regex_dict.items()}
+
+    if not efs['location']:
+        return False
+
+    cfs = {f: _clean_extract(v) for f, v in efs.items()}
+
+    pc = extract_postcode(cfs['location'])
+    ac = extract_areacode(pc) or extract_areacode(cfs['location'])
+    gd = geocode_dic(cfs['location'], pc, ac)
+
+    cfs['date_posted'] = dt.strptime(cfs['date_posted'], '%d/%m/%Y')
+    try:
+        ex = {'size_avg': (cfs['size_min'] + cfs['size_max']) / 2}
+    except Exception:
+        ex = {'size_avg': None}
+
+    rld = {**cfs, **ex, **gd}
+    rld['email_id'] = uid
+
+    l = Disposal(**rld)
+    db.session.add(l)
+    db.session.commit()
+    return True
+
+
+def del_all():
+    Disposal.query.delete()
+
+
+def all_data():
+    qry = db.session.query(Disposal).all()
+    d = pd.DataFrame([l.to_dict() for l in qry])
+    d.index.name = 'id'
+    return d
+
+# del_all()
+update_database()
